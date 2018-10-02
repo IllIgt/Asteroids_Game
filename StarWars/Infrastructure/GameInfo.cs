60c92e8963ee9091c7de37e3a9693360d28b300c
IllIgt <Voblin_ne@mail.ru>
2018-10-03 02:16:54 +0300

    Lesson4

---
+++ StarWars/Infrastructure/GameInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars.Infrastructure
+{
+    class GameInfo
+    {
+        public string PlayerName { get; set; }
+        public int ScoresCount { get; set; }
+        public int DestroyedAsteroids { get; set; }
+        public override string ToString()
+        {
+            return $"{PlayerName} : scores {ScoresCount}; asteroids : {DestroyedAsteroids}";
+        }
+    }
+
+    public class GameInfoStorage : IEnumerable<GameInfo>
+    {
+        private ICollection<GameInfo> _Games;
+
+        public IEnumerator<GameInfo> GetEnumerator()
+        {
+            foreach (var info in _Games)
+                yield return info;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
